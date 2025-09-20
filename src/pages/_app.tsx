f12a87bda9504db49157207266c5ded40e8ae445
altikrivanjeva <altikrivanjeva@gmail.com>
2025-09-20 18:43:39 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import type { AppProps } from 'next/app';
+import { AuthProvider } from '../context/AuthContext';
 import '../styles/globals.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+
+export default function App({ Component, pageProps }) {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
