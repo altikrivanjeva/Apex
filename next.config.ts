7666bccef7d4b9d2695da0f6bacdb790bf1049c6
Roni <Ronifetahu01@gmail.com>
2026-04-02 23:57:28 +0200

    update
    fotot

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,26 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'vitafit-ks.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.preworkout.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'c4preworkout.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'a1protein.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
